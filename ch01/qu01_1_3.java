43f1259a431aebc29cd413cb8b9ac72f350d0f1c
j-hoon <joen7619@gmail.com>
2017-09-23 04:14:08 +0900

    170923_2

---
+++ ch01/qu01_1_3.java
@@ -0,0 +1,11 @@
+class qu01_1_3
+{
+	public static void main(String[] args)
+	{
+		System.out.println(12);
+		System.out.println("12");
+		System.out.println("1" + "2");
+		System.out.println("1" + 2);
+		System.out.println(1 + "2");
+	}
+}
